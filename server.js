7215d46b6d70cbf77db16af861b65c2dd960daba
kuwar jha <kuwarjha.cse@gmail.com>
2025-05-09 18:03:48 +0530

    added cookies header credential in cors

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express();
 // Middleware
 const corsOptions = {
   origin: process.env.FRONTEND_URL || 'http://localhost:9002',
+  credentials: true,
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
