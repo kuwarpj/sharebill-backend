8fcd6678ae82a53144a68d2c54c551fe1a5f3f99
kuwarpj <kuwarjha.cse@gmail.com>
2025-07-10 15:24:20 +0530

    fix authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      
+
       // Add user id to request object (you might fetch full user from DB here)
-      req.user = { id: decoded.id }; 
-      
+      req.user = await User.findById(decoded.id).select("-password");
+     
+
       next();
     } catch (error) {
-      console.error('Token verification failed:', error);
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      console.error("Token verification failed:", error);
+      res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+    res.status(401).json({ message: "Not authorized, no token" });
   }
 };
 
