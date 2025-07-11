b401141e0f95235b70827cec2bf279d532cc3c93
kuwarpj <kuwarjha.cse@gmail.com>
2025-07-11 14:41:06 +0530

    added personal expense model

---
+++ models/personalExpense.mo.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const personalExpenseSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0.01,
+    },
+    paidBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const personalExpense = mongoose.model(
+  "personalExpense",
+  personalExpenseSchema
+);
+
+export default personalExpense;
